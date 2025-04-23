4e8b580fdf9e8bdb6aa1cdd5ed9b16e233171e2e
krekab16 <kunreka2001@gmail.com>
2025-04-23 08:50:24 +0200

    file modositas

--- cosine_similarity_embedding.py
+++ cosine_similarity_embedding.py
@@ -1,13 +1,10 @@
 
-from google.colab import drive
 import os
 from google.cloud import firestore
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
-drive.mount('/content/drive')
-
-file_path = "/content/drive/MyDrive/Szakdolgozat_MSc/szakdolgozat-a9498-c9d93c04fb55.json"
+file_path = "credentials.json" 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = file_path
 firestore_client = firestore.Client()
 
